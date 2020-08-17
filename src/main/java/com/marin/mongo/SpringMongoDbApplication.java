acd3d8d2971d877aee33e1208aad71051666adc5
MarinPuizina <marinpuizina@hotmail.com>
2020-08-17 22:06:46 +0200

    Add initial commit

---
+++ src/main/java/com/marin/mongo/SpringMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.marin.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongoDbApplication.class, args);
+	}
+
+}
