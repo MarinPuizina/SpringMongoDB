2e4cd3f9bf177f4922e174e88a63e872355e8c4c
MarinPuizina <marinpuizina@hotmail.com>
2020-08-17 22:28:59 +0200

    Add test for repository

---
+++ src/test/java/com/marin/mongo/space/SpaceShipRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.marin.mongo.space;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class SpaceShipRepositoryTest {
+
+    @Autowired
+    SpaceShipRepository spaceShipRepository;
+
+    @Test
+    public void createRows() {
+
+        // Preparing the database
+        spaceShipRepository.deleteAll();
+
+        spaceShipRepository.save(new SpaceShip(null, "Ship A", 1));
+        spaceShipRepository.save(new SpaceShip(null, "Ship B", 2));
+        spaceShipRepository.save(new SpaceShip(null, "Ship C", 3));
+        spaceShipRepository.save(new SpaceShip(null, "Ship D", 4));
+        spaceShipRepository.save(new SpaceShip(null, "Ship E", 5));
+
+        spaceShipRepository.findAll().forEach(System.out::println);
+
+    }
+
+}
