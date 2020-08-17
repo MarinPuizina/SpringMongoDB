0c9314a58f877b1b88e03f17fd9a8f8721e743c5
MarinPuizina <marinpuizina@hotmail.com>
2020-08-18 00:14:47 +0200

    Add allSpacesShips() to query MongoDB to SpaceShipService

--- src/main/java/com/marin/mongo/space/SpaceShipService.java
+++ src/main/java/com/marin/mongo/space/SpaceShipService.java
@@ -1,10 +1,30 @@
 package com.marin.mongo.space;
 
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class SpaceShipService {
+
+    private final MongoTemplate mongoTemplate;
+
+    public SpaceShipService(MongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    public List<SpaceShip> allSpacesShips() {
+
+        Query query = new Query()
+                .addCriteria(Criteria.where("Type").is("Ship A"))
+                .with(Sort.by(Sort.Order.desc("Type")))
+                .limit(2);
+
+        return mongoTemplate.find(query, SpaceShip.class);
+    }
 
 }
