875ad4018c9ae8b09d835e861a76bd3048b03ce3
MarinPuizina <marinpuizina@hotmail.com>
2020-08-20 00:17:47 +0200

    Add test for spacestations

--- src/test/java/com/marin/mongo/spacestation/SpaceStationRepositoryTest.java
+++ src/test/java/com/marin/mongo/spacestation/SpaceStationRepositoryTest.java
@@ -1,10 +1,20 @@
 package com.marin.mongo.spacestation;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class SpaceStationRepositoryTest {
+
+    @Autowired
+    SpaceStationRepository spaceStationRepository;
+
+    @Test
+    public void testSpaceStation() {
+        spaceStationRepository.findAll().forEach(System.out::println);
+    }
 
 }
