8192f78a307979a3d519dd091b9235f06cb396ff
MarinPuizina <marinpuizina@hotmail.com>
2020-08-19 21:44:21 +0200

    Add SpaceStationRepositoryTest.java

---
+++ src/test/java/com/marin/mongo/spacestation/SpaceStationRepositoryTest.java
@@ -0,0 +1,10 @@
+package com.marin.mongo.spacestation;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class SpaceStationRepositoryTest {
+
+}
