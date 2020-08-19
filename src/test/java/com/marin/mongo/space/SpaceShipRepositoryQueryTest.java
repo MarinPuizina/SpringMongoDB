ae582efbbdcde3b74540e417ccb317c923ada8c1
MarinPuizina <marinpuizina@hotmail.com>
2020-08-19 20:34:20 +0200

    Add test for queries

--- src/test/java/com/marin/mongo/space/SpaceShipRepositoryQueryTest.java
+++ src/test/java/com/marin/mongo/space/SpaceShipRepositoryQueryTest.java
@@ -1,10 +1,33 @@
 package com.marin.mongo.space;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class SpaceShipRepositoryQueryTest {
+
+    @Autowired
+    SpaceShipRepository spaceShipRepository;
+
+    @Test
+    public void testGetShipsStartWithB() {
+
+        List<SpaceShip> spaceships = spaceShipRepository.getShipsStartWithB();
+        spaceships.forEach(System.out::println);
+
+    }
+
+    @Test
+    public void testGetShipsStartWithS() {
+
+        List<SpaceShip> spaceships = spaceShipRepository.getShipsStartWithS();
+        spaceships.forEach(System.out::println);
+
+    }
 
 }
