2a17aca7b0062b059ed347dd8054e83900a84f5c
MarinPuizina <marinpuizina@hotmail.com>
2020-08-19 20:16:12 +0200

    Add SpaceShipRepositoryQueryTest.java

---
+++ src/test/java/com/marin/mongo/space/SpaceShipRepositoryQueryTest.java
@@ -0,0 +1,10 @@
+package com.marin.mongo.space;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class SpaceShipRepositoryQueryTest {
+
+}
