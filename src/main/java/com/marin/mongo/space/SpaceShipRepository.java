c389144e02bed7ab5166c29b82829e117a0e1ae1
MarinPuizina <marinpuizina@hotmail.com>
2020-08-19 14:01:32 +0200

    Add queries

--- src/main/java/com/marin/mongo/space/SpaceShipRepository.java
+++ src/main/java/com/marin/mongo/space/SpaceShipRepository.java
@@ -1,6 +1,17 @@
 package com.marin.mongo.space;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface SpaceShipRepository extends MongoRepository<SpaceShip, String> {
+
+    @Query("{'type': {$regex: '^B.*'} }")
+    public List<SpaceShip> getShipsStartWithB();
+
+
+    @Query("{'type': {$regex: '^S.*'} }")
+    public List<SpaceShip> getShipsStartWithS();
+
 }
